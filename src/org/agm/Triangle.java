117f14c05666983b0a7ed7252b68363ef736bb3f
Aishwarya-G-M <aishwarya.govindappamahadev@gmail.com>
2026-09-08 20:20:14 +0530

    Familiarize with Aware interfaces - ApplicationContextAware,BeanNameAware

--- src/org/agm/Triangle.java
+++ src/org/agm/Triangle.java
@@ -1,8 +1,15 @@
 package org.agm;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
     private String type;
     private String color;
+    ApplicationContext context = null;
+    String beanName;
 
     public Triangle() {
     }
@@ -25,5 +32,19 @@ public class Triangle {
 
     public void draw(){
         System.out.println(getType()+" Triangle Drawn with color "+getColor());
+    }
+
+    public void showBeanName(){
+        System.out.println("The class bean is called = "+this.beanName);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        this.beanName = s;
     }
 }
